9800fddd1a44551233cf650121c8ace17b5694f0
Ebinu-s <ebinusuneer2nd@gmail.com>
2023-05-25 12:33:38 +0530

    add: menu for mobile, animation init, chisp no wrap bug fix

--- src/components/nav/index.js
+++ src/components/nav/index.js
@@ -1,8 +1,28 @@
-import React from 'react';
+import { useState } from 'react';
 import styles from './index.module.scss';
 import Link from 'next/link';
+import Image from 'next/image';
+
+import iconmail from '@/assets/email.png';
+import iconmenu from '@/assets/menu.svg';
+import { AnimatePresence, motion, useAnimate } from 'framer-motion';
 
 function Nav() {
+	const [openMenu, setOpenMenu] = useState(false);
+
+	function toggleMenu() {
+		const tempState = !openMenu;
+
+		if (typeof window != 'undefined' && window.document) {
+			if (tempState) {
+				document.body.style.overflow = 'hidden';
+			} else {
+				document.body.style.overflow = 'auto';
+			}
+		}
+		setOpenMenu(tempState);
+	}
+
 	return (
 		<nav className={styles.nav}>
 			<div className={styles.navContainer}>
@@ -19,10 +39,39 @@ function Nav() {
 					<Link href={'/works'}>
 						<span className={styles.pageLink}>Work</span>
 					</Link>
+					<div className={styles.contactContainer}>
+						<Image src={iconmail} width={15} height={15} />
+						<a href='mailto:ebinusuneer2nd@gmail.com'>Contact</a>
+					</div>
 				</div>
-
-				<button className='primary-btn'>Lets Connect</button>
+				{/* mobile menu */}
+				<button className={styles.menuButton} onClick={toggleMenu}>
+					<Image src={iconmenu} width={15} height={15} />
+				</button>
 			</div>
+			<AnimatePresence>
+				{openMenu && (
+					<motion.div
+						className={styles.mobileMenu}
+						initial={{ opacity: 0, y: '-100vh' }}
+						animate={{ y: 0, opacity: 1 }}
+						exit={{ opacity: 0 }}>
+						<Link href={'/'}>
+							<span className={styles.pageLink}>Home</span>
+						</Link>
+						<Link href={'/about'}>
+							<span className={styles.pageLink}>About</span>
+						</Link>
+						<Link href={'/works'}>
+							<span className={styles.pageLink}>Work</span>
+						</Link>
+						<div className={styles.contactContainer}>
+							{/* <Image src={iconmail} width={40} height={40} /> */}
+							<a href='mailto:ebinusuneer2nd@gmail.com'>Contact</a>
+						</div>
+					</motion.div>
+				)}
+			</AnimatePresence>
 		</nav>
 	);
 }
